eff1597d5a3bb5e03b1ae3586183c81b2430e62f
Jonathan <Jonathan@192.168.1.7>
2023-04-08 22:06:37 -0300

    abc

---
+++ projetotestegh/src/projetoTesteGH/projetoJdev.java
@@ -0,0 +1,35 @@
+package projetoTesteGH;
+
+public class projetoJdev {
+
+
+
+	
+	/**algoritmo 98 ...
+A prefeitura do Rio de Janeiro abriu uma linha de crédito para os funcionários estatutários. O valor máximo da prestação não
+poderá ultrapassar 30% do salário bruto. Fazer um algoritmo que permita entrar com o salário bruto e o valor da 
+prestação e informar se o empréstimo pode ou não ser concedido. */
+
+   public static void main(String[] args) {
+	   
+	double salario = 1000;
+	double valorEmprestimoMaximo = (salario/100) * 30;
+	double prestacao = 600 ;
+	
+		
+	if(prestacao <= valorEmprestimoMaximo) {
+		
+		System.out.println("Autorizado o emprestimo de R$ " + prestacao);
+	} else {
+		 
+		System.out.println("NÃO Autorizado. Valor máximo de emprestimo é de 30% do salário R$ " + valorEmprestimoMaximo);
+		
+	}
+	 
+		
+	 
+  }
+
+}
+
+
