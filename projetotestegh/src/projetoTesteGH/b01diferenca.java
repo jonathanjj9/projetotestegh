8182f55c7c10197d80cd694abc1340109496b8ce
Jonathan <Jonathan@192.168.1.3>
2023-04-16 10:46:32 -0300

    diferenca do maior para o menor

---
+++ projetotestegh/src/projetoTesteGH/b01diferenca.java
@@ -0,0 +1,35 @@
+package projetoTesteGH;
+
+public class b01diferenca {
+
+		
+		/**Efetuar a leitura de dois numeros inteiros representados pelas variaveis
+		e apresentar o resultado da diferença do maior valor pelo menor valor*/
+		
+		
+		public static void main(String[] args) {
+			
+			int a= 5;
+			int b= 30; 
+			int x ;
+			
+			
+			
+			if (a > b) { 
+				
+				x = a-b;
+				System.out.println(x);
+			}
+			
+			else {
+				x = b-a;
+				System.out.println(x);
+			}
+		}
+
+	}
+
+
+
+	
+
