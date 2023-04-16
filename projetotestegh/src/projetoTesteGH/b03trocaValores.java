0deb86b6feed720b82e728dff1d8b99074a213ba
Jonathan <Jonathan@192.168.1.3>
2023-04-16 11:23:14 -0300

    Trocando valores entre variaveis

---
+++ projetotestegh/src/projetoTesteGH/b03trocaValores.java
@@ -0,0 +1,25 @@
+package projetoTesteGH;
+
+public class b03trocaValores {
+
+
+ 
+	
+	public static void main(String args[]) {
+		
+		int a = 90;
+		int b = 50;
+		int inverte = a ;
+		
+		
+		a = b;
+		b = inverte;
+		
+		System.out.println(" ******** Valores invertidos ******** \n");
+	    System.out.println("Novo valor de A: " + a);
+		System.out.println("Novo valor de B: " + b);
+		
+	}
+	
+}
+
