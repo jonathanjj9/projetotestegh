c9cb64c03038550a266d5526d3b6c51a79cbeb1a
Jonathan <Jonathan@192.168.1.3>
2023-04-16 11:46:09 -0300

    Calculando volume de uma lata

---
+++ projetotestegh/src/projetoTesteGH/b04volumeLata.java
@@ -0,0 +1,21 @@
+package projetoTesteGH;
+
+ /* Calcular volume de uma lata usando a seguinte formula: raio ao quadrado  * pi  * altura */
+
+public class b04volumeLata {
+
+	public static void main(String args[]) {
+		
+		double volume;
+		double r = 1.2; 
+		double altura = 2;
+		
+		volume = 3.14 * (r*r) * altura;
+		
+		System.out.println("Volume da lata: " + volume);
+		
+		
+		
+		
+		}	
+}
