696493e722171399bf43f299f3a92868943f9d06
Jonathan <Jonathan@192.168.1.3>
2023-04-16 10:57:16 -0300

    Média de 4 notas

---
+++ projetotestegh/src/projetoTesteGH/b02mediaNota.java
@@ -0,0 +1,29 @@
+package projetoTesteGH;
+
+public class b02mediaNota {
+
+
+
+	
+	public static void main(String[] args) {
+		
+		double n1= 6, n2= 7,n3= 7, n4= 10, media = (n1+n2+n3+n4)/4;
+		
+		if (media >= 7) { 
+			
+			
+			System.out.println("Aprovado " );
+			System.out.println("Média "+ media );
+		}
+		
+		else {
+			System.out.println("Reprovado " );
+			System.out.println("Média "+ media );
+
+    }
+ 
+  }
+ 
+}
+	
+
