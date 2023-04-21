b029d269a99b699c1e4f276b7ebf21bd21c38ad2
Jonathan <Jonathan@192.168.1.4>
2023-04-21 12:27:49 -0300

    Calculando gasto de combustivel

---
+++ projetotestegh/src/projetoTesteGH/b05combustivel.java
@@ -0,0 +1,20 @@
+package projetoTesteGH;
+
+public class b05combustivel {
+
+public static void main(String[] args) {
+	
+
+	double tempoHr= 1.45 , velocidadeKM = 120, distancia, litrosUsados;
+	
+	distancia = tempoHr * velocidadeKM;
+	litrosUsados = distancia / 12.0;
+	
+	System.out.println("******* *********** ********** \n" + "       Combustivel Gasto \n" 
+	+ "******* *********** **********" );
+	
+	System.out.println("Velocidade Média: " + velocidadeKM + " km \n" + "Tempo gasto: " + tempoHr + 
+	" hora/min \n" + "Quilometragem: " + distancia + " km \n" + "Litros consumido: " + litrosUsados + " lt" );	
+	
+   }
+}
