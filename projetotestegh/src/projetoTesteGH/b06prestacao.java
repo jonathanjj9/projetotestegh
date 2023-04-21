318ead0514178f6a78251303b9dbecfde8d44086
Jonathan <Jonathan@192.168.1.4>
2023-04-21 16:09:34 -0300

    multa de 2% ao mês

--- projetotestegh/src/projetoTesteGH/b06prestacao.java
+++ projetotestegh/src/projetoTesteGH/b06prestacao.java
@@ -9,13 +9,15 @@ public class b06prestacao {
 
 	public static void main(String[] args) {
 
-		double prestacao, valor = 138.59, multa = 2, mes = 50;
+		double prestacao, valor = 138.59, multa = 2;
+		int mes = 50;
 
 		multa = valor * (mes / 100);
 
 		prestacao = valor + (valor * (multa / 100));
 
 		System.out.println("Valor bruto: R$ " + valor);
-		System.out.printf("Valor corrigido com multa: R$ %.2f ", +prestacao);
+		System.out.printf("Valor corrigido com multa: R$ %.2f\n", +prestacao);
+		System.out.println("Meses pendentes: " + mes);
 	}
 }
