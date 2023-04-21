f1de7ff11fac81d8d5a4dc27c90dce2e7465abb7
Jonathan <Jonathan@192.168.1.4>
2023-04-21 16:04:23 -0300

    Prestação com correção de juros e multa

--- projetotestegh/src/projetoTesteGH/b06prestacao.java
+++ projetotestegh/src/projetoTesteGH/b06prestacao.java
@@ -7,17 +7,15 @@ package projetoTesteGH;
 
 public class b06prestacao {
 
-	 public static void main(String[] args) {
-		
-		
-			
-			double prestacao, valor = 138.59, multa = 2,  mes = 6;
-			
-			multa = valor*(mes/100);
-			
-			prestacao = valor +(valor*(multa/100));
-			
-			System.out.println("Valor Bruto: " + valor);
-			System.out.printf("Valor corrigido com multa: %.2f ", + prestacao );
+	public static void main(String[] args) {
+
+		double prestacao, valor = 138.59, multa = 2, mes = 50;
+
+		multa = valor * (mes / 100);
+
+		prestacao = valor + (valor * (multa / 100));
+
+		System.out.println("Valor bruto: R$ " + valor);
+		System.out.printf("Valor corrigido com multa: R$ %.2f ", +prestacao);
 	}
 }
