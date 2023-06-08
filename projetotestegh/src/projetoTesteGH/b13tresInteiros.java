decc63cd9ba1f065b7c381e15a623bc9cbdd9870
Jonathan <Jonathan@192.168.1.3>
2023-06-08 15:26:04 -0300

    Imprimir 3 numeros diferentes ao quadrado.

---
+++ projetotestegh/src/projetoTesteGH/b13tresInteiros.java
@@ -0,0 +1,22 @@
+package projetoTesteGH;
+
+/** imprima 3 numeros diferentes ao quadrado.**/
+
+public class b13tresInteiros {
+	
+public static void main(String[] args) {
+		
+		int a = 10, b = 20, c = 30;
+	
+		double quadrado1 = a*a;
+		double quadrado2 = b*b;
+		double quadrado3 = c*c;
+		
+		System.out.print("A ao quadrado: " + quadrado1);
+		System.out.print(" - B ao quadrado: " + quadrado2);
+		System.out.print(" - C ao quadrado: " + quadrado3);
+		
+		
+	}
+
+}
