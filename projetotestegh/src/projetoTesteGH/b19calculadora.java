f641828091db150d66ea927d3524af6e390f4a70
Jonathan <Jonathan@192.168.1.4>
2023-06-10 17:56:09 -0300

    Calculadora simples.

---
+++ projetotestegh/src/projetoTesteGH/b19calculadora.java
@@ -0,0 +1,25 @@
+package projetoTesteGH;
+/**Criar um programa que calcule o valor de duas variaveis, usando as 4 principais operações 
+ * aritimeticas. (Soma, subtração, multiplicação e divisão**/
+
+public class b19calculadora {
+
+public static void main (String[] args) {
+		
+		double a = 8;
+		double b = 5;
+		
+		double resposta1 = a+b;
+		double resposta2 = a-b;
+		double resposta3 = a*b;
+		double resposta4 = a/b;
+		
+		System.out.println(resposta1);
+		System.out.println(resposta2);
+		System.out.println(resposta3);
+		System.out.println(resposta4);
+		
+		
+		
+	}
+}
