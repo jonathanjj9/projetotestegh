b992411db99c146d4058a9dad1880ed808f4a779
Jonathan <Jonathan@192.168.1.4>
2023-06-10 16:31:41 -0300

    Calculo do raio de uma circunferencia

---
+++ projetotestegh/src/projetoTesteGH/b17circunferencia.java
@@ -0,0 +1,14 @@
+package projetoTesteGH;
+
+public class b17circunferencia {
+
+public static void main(String[] args) {
+	
+	double area;
+	double raioC = 5.35;  	
+	
+	area = 3.14* (raioC * raioC);
+	
+	System.out.println("area da circuferência: " + area);
+ }
+}
