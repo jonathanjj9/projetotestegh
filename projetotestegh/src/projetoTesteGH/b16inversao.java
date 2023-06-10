84d651e78836f9d5904423aed951cd8f56916673
Jonathan <Jonathan@192.168.1.4>
2023-06-10 12:27:31 -0300

    Troca de valores entre variaveis

---
+++ projetotestegh/src/projetoTesteGH/b16inversao.java
@@ -0,0 +1,19 @@
+package projetoTesteGH;
+
+public class b16inversao {
+
+	public static void main(String args[]) {
+		
+		int a = 10;
+		int b = 20;
+		int inverte = a ;
+		
+		a = b;
+		b = inverte;
+		
+		System.out.println(" ******** Valores invertidos ******** \n");
+	    System.out.println("Novo valor de A: " + a);
+		System.out.println("Novo valor de B: " + b);
+		
+	}
+}
