33d7740bbcd6c1e0ed4796bb52813ec0770a6f54
Jonathan <Jonathan@192.168.1.4>
2023-06-11 10:08:29 -0300

    Calculo do volume de uma esfera

---
+++ projetotestegh/src/projetoTesteGH/b21esfera.java
@@ -0,0 +1,14 @@
+package projetoTesteGH;
+/* calculo do volume de uma esfera */
+
+public class b21esfera {
+
+	public static void main(String[] args) {
+		
+		double raio = 2.53;
+		double volume = (4/3)*3.14*(raio*raio*raio);
+		
+		System.out.println(volume);
+		
+	}
+}
