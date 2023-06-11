f355fb236d72041c02cba7c08322bb27148b5d27
Jonathan <Jonathan@192.168.1.4>
2023-06-11 10:34:09 -0300

    Imprimir na tela o antecessor e suessor de numero amazenado em memoria.

---
+++ projetotestegh/src/projetoTesteGH/b22sucessorAntecessor.java
@@ -0,0 +1,20 @@
+package projetoTesteGH;
+ /*mostrar na tela o antecessor e suessor de numero amazenado em memoria.*/
+
+public class b22sucessorAntecessor {
+	
+	public static void main(String[] args) {
+		
+		double numero = 101;
+		double antecessor ;
+		double sucessor ; 
+		
+		sucessor = numero + 1; 
+		antecessor = numero - 1; 
+		
+		System.out.println("Antecessor: " + antecessor);
+		System.out.println("Sucessor: " + sucessor);
+	 }
+
+
+}
