e733a66c643209d89f1ae0b7abaa70b17ebf0443
Jonathan <Jonathan@192.168.1.4>
2023-06-11 09:14:32 -0300

    Calculo de velocidade por minuto.

---
+++ projetotestegh/src/projetoTesteGH/b20velocidadeMinuto.java
@@ -0,0 +1,15 @@
+package projetoTesteGH;
+
+/**Calculo de velocidade por minuto**/
+public class b20velocidadeMinuto {
+
+		
+		public static void main(String[] args) {
+			
+			double tempoMinuto = 17;
+			double distanciaKm = 80;
+			double velocidade = (distanciaKm * 1000) / (tempoMinuto * 60);
+			
+			System.out.println(velocidade);
+		}
+}
