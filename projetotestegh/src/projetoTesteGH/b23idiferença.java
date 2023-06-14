6389c695e9da5f5c437d2c61ba17af11c1116dfb
Jonathan <Jonathan@192.168.1.4>
2023-06-13 22:15:49 -0300

    diferença de um numero subtraido pelo outro.

---
+++ projetotestegh/src/projetoTesteGH/b23idiferença.java
@@ -0,0 +1,19 @@
+package projetoTesteGH;
+
+public class b23idiferença {
+
+	public static void main(String[] args) {
+
+		int n1 = 50;
+		int n2 = 20;
+
+		if (n1 >= n2) {
+
+			System.out.println(n1 - n2);
+
+		} else {
+
+			System.out.println(n2 - n1);
+		}
+	}
+}
