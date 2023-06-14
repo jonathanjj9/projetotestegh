5e353d6157212f852a3750fcf25389d94f28ebe9
Jonathan <Jonathan@192.168.1.4>
2023-06-13 22:28:27 -0300

    Criando e imprimindo strings

---
+++ projetotestegh/src/projetoTesteGH/b24nome.java
@@ -0,0 +1,15 @@
+package projetoTesteGH;
+
+public class b24nome {
+
+	public static void main(String[] args) {
+
+		String nome = "Simão";
+		String endereco = "R. Neve na Bahia - José Bonifacio, São Paulo - SP, 08257-110" ;
+		String telefone = "+55 11 4566-3558";
+
+		System.out.println("nome: " + nome);
+		System.out.println("Endereço: " + endereco);
+		System.out.println("Telefone: " + telefone);
+	}
+}
