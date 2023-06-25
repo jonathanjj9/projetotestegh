42d16d2a9cc78e0d410b0c07bcdb976c8c3ad97f
Jonathan <Jonathan@192.168.1.3>
2023-06-25 10:55:37 -0300

    Fazer um algoritmo que possa entrar com o saldo de uma aplicação e imprima o 	novo saldo, considerando o reajuste de 1 %.

---
+++ projetotestegh/src/projetoTesteGH/b28reajusteUm.java
@@ -0,0 +1,20 @@
+package projetoTesteGH;
+
+public class b28reajusteUm {
+
+	/**Fazer um algoritmo que possa entrar com o saldo de uma aplicação e imprima o 
+	novo saldo, considerando o reajuste de 1 %. */
+	
+	public static void main(String[] args) {
+		
+		double saldoAplicacao = 500;
+		double valorPorcentagem = 1;
+		double percentual = (saldoAplicacao*valorPorcentagem)/100;
+		
+		System.out.println(percentual);
+		
+		
+	}
+	
+	
+}
