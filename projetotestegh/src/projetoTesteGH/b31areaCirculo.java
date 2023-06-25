4b15852c11bd3e2d49a4664361b3f3df16d22837
Jonathan <Jonathan@192.168.1.3>
2023-06-25 11:28:04 -0300

    area de um circulo

---
+++ projetotestegh/src/projetoTesteGH/b31areaCirculo.java
@@ -0,0 +1,19 @@
+package projetoTesteGH;
+
+public class b31areaCirculo {
+
+	public static void main(String[] args) {
+
+		double raio = 3;
+		double perimetro;
+		double area;
+
+		perimetro = 2 * 3.14 * raio;
+		area = 3.14 * raio;
+
+		System.out.println("perimetro: " + perimetro);
+		System.out.println("area: " + area);
+
+	}
+
+}
