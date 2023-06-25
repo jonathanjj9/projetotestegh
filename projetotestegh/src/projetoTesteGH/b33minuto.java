8be5ad50a1fe357e4a187499a180fa6f5fa04871
Jonathan <Jonathan@192.168.1.3>
2023-06-25 11:35:00 -0300

    Minutos passados desde o inicio do dia

--- projetotestegh/src/projetoTesteGH/b33minuto.java
+++ projetotestegh/src/projetoTesteGH/b33minuto.java
@@ -2,4 +2,17 @@ package projetoTesteGH;
 
 public class b33minuto {
 
+	/**
+	 * Criar um algoritmo que leia um valor de hora e informe quantos minutos se
+	 * passsaram desde o início do dia.
+	 */
+
+	public static void main(String[] args) {
+
+		double umaHora = 0.6 * 100;
+		double horaAtual = 3;
+		double minutosPassados = umaHora * horaAtual;
+
+		System.out.println("Até o momento se passaram: " + minutosPassados + " Minutos. ");
+	}
 }
